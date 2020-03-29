0e1b7e07b2e2761f7ffafba463ad98d71a4fce4b
Benjamin Calef <zepgram@gmail.com>
2020-03-29 18:11:29 +0200

    add acceptance job

--- .github/push.sh
+++ .github/push.sh
@@ -5,7 +5,7 @@ if [ "$(git diff --name-status --exit-code)" ]; then
   git config --local user.name "$2"
   git commit -am "$3"
   git remote set-url origin "$(git config --get remote.origin.url | sed 's#http.*com/#git@github.com:#g')"
-  mkdir -p ~/.ssh/ && echo "$4">~/.ssh/id_rsa && chmod 400 ~/.ssh/.id_rsa
+  mkdir -p ~/.ssh/ && echo "$4">~/.ssh/id_rsa && chmod 400 ~/.ssh/id_rsa
   ssh-keyscan github.com >>~/.ssh/known_hosts
   git push
   exit 1
