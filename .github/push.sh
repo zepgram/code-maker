b8bf1a320cc1a39a0a42ec4529098eb1bc5a67ee
Benjamin Calef <zepgram@gmail.com>
2020-03-29 17:46:37 +0200

    add acceptance job

---
+++ .github/push.sh
@@ -0,0 +1,11 @@
+#!/bin/sh
+
+if ! git diff --no-ext-diff --quiet --exit-code; then
+  git config --local user.email "$1"
+  git config --local user.name "$2"
+  git commit -am "$3"
+  git remote set-url origin "$(git config --get remote.origin.url | sed 's#http.*com/#git@github.com:#g')"
+  mkdir -p ~/.ssh/
+  ssh-keyscan github.com >>~/.ssh/known_hosts
+  git push
+fi
